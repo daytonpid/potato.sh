c49e221dbd77558aed6f5e63e1f212637694eee3
daytonpid <deepsixsec@protonmail.com>
2017-12-29 02:05:52 -0700

    adding base files for new website and a little demo

---
+++ src/pelicanconf.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from os import getcwd, path
+
+# PELICAN_OPTS
+AUTHOR = 'd$pid'
+AUTHOR_REAL = 'Dayton Pidhirney'
+SITENAME = 'potato.sh'
+SITEURL  = 'https://potato.sh'
+PATH     = 'content'
+TIMEZONE = 'America/Edmonton'
+DEFAULT_LANG = 'en'
+
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+OUTPUT_PATH = path.join(getcwd().strip("src"), "webpage")
+DELETE_OUTPUT_DIRECTORY = True
+
+
+# Blogroll
+LINKS = (('Seekintoo', 'https://www.seekintoo.com'),)
+
+# Social widget
+SOCIAL = (('github', 'https://github.com/daytonpid'),
+          ('twitter', 'https://twitter.com/d$pid'))
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+# THEME_OPTS
+DISPLAY_TAGS_ON_SIDEBAR_LIMIT = 0
+DISPLAY_LINKS_ON_SIDEBAR_LIMIT = 0
+STYLE = "gradient"
+CAPITALIZE_HEADINGS = True
+LICENSE = {
+    'cc_name':"by-sa",
+    'hosted':False,
+    'compact':True,
+    'brief':False
+}
